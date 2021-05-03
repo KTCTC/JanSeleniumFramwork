a797fbac52b48259a5e933db11f733b179889c16
KTCTC <ktcorporatetrainingcentre@gmail.com>
2021-05-03 19:55:51 +0530

    sample RR

--- Framwork-master/src/test/java/KTCTC/RR.java
+++ Framwork-master/src/test/java/KTCTC/RR.java
@@ -12,13 +12,6 @@ public class RR {
 	public void sampleMethod2()
 	{
 		System.out.println("Hello ...");
-		System.out.println("Hello Ganesh");
-		System.out.println("updated by SK");
-        System.out.println("Updated by Pravin");
-		System.out.println("123");
-		System.out.println("+++++++++++++++++++++");
-		System.out.println("1234567890");
-		System.out.println("KTCTC Git");
-
+		
 	}
 }
